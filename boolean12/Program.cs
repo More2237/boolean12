4f29bd811c1eb4d51e8220b6fe489e0dd1cafb27
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-24 11:45:25 +0500

    hugh

---
+++ boolean12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace boolean12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int a = int.Parse(ReadLine());
+         int b = int.Parse(ReadLine());
+         int c = int.Parse(ReadLine());
+         
+         bool result = a > 0 && b > 0 &&c > 0;
+        
+         
+         WriteLine(result);
+         
+         
+        }
+    }
+}
